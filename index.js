ce99cc56d3b6a0637d003d821456433365c58c32
Artem Smirnov <aratatem2013@gmail>
2021-03-05 10:46:10 +0200

    додає pad

--- index.js
+++ index.js
@@ -22,12 +22,16 @@ class CountdownTimer {
   }
 
   getTimeComponents(time) {
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
     return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
   }
 }
 
@@ -44,4 +48,4 @@ const timer = new CountdownTimer({
   onTick: updateTimer,
 });
 
-console.log(timer.start());
+timer.start();
